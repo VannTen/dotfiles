1d8eaa144cc8d5895457e8dd51daab49665399f5
Max Gautier <mg@max.gautier.name>
2022-07-26 11:10:29 +0200

    Using the git prompt included with git

--- .bashrc
+++ .bashrc
@@ -11,4 +11,3 @@ do
 done
 
 alias ls='ls --color=auto'
-PS1='[\u@\h $(kube_ps1) \W]\$ '
---
+++ .config/bash/prompt.bash
@@ -0,0 +1,5 @@
+source /usr/share/git/git-prompt.sh
+PROMPT_COMMAND='__git_ps1 "[\u@\h \w]" "\\\$ "'
+GIT_PS1_SHOWCOLORHINTS=1
+GIT_PS1_SHOWDIRTYSTATE=1
+GIT_PS1_SHOWUPSTREAM=auto
