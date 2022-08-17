9f0a85e4bd148521b492f84235e9db71ba27e657
Max Gautier <mg@max.gautier.name>
2022-08-17 16:28:53 +0200

    unlimited bash history

--- .config/bash/history.bash
+++ .config/bash/history.bash
@@ -1,1 +1,10 @@
-HISTCONTROL="ignoreboth:erasedups"
+HISTCONTROL="ignoreboth"
+HISTSIZE=10000000
+HISTFILESIZE="" # Disable history file cleanup
+HISTTIMEFORMAT="%F %T  " # Timestamps in history file
+
+shopt -s histappend
+shopt -s cmdhist
+
+bind '"\e[B": history-search-forward'   # instead of just walking the history,
+bind '"\e[A": history-search-backward'  # perform a search on <UP>/<DOWN>
--- .config/bash/prompt.bash
+++ .config/bash/prompt.bash
@@ -1,5 +1,6 @@
 source /usr/share/git/git-prompt.sh
 PROMPT_COMMAND='__git_ps1 "[\u@\h \w]" "\\\$ "'
+PROMPT_COMMAND="$PROMPT_COMMAND; history -a"
 GIT_PS1_SHOWCOLORHINTS=1
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWUPSTREAM=auto
