e66123fcf2ba90208adb246fdfa3f139f9c8acc1
Max Gautier <mg@max.gautier.name>
2022-11-18 13:05:47 +0100

    Use device.serial instead of device.name to disable
    The latter is apparently not stable

--- .config/wireplumber/main.lua.d/51-disable-dock-analog.lua
+++ .config/wireplumber/main.lua.d/51-disable-dock-analog.lua
@@ -1,7 +1,7 @@
 rule = {
   matches = {
     {
-      { "device.name", "equals", "alsa_card.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00" },
+      { "device.serial", "equals", "Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000" },
     },
   },
   apply_properties = {
